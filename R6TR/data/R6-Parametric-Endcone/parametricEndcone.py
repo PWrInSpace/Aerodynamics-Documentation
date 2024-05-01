bc5e9d1697c979bbe97f628916d40ec7c9c93233
Manfred Gawlas <manfredgawlas12@gmail.com>
2024-05-01 12:52:51 +0200

    Final version of Optimalisation of Endcone in Solidworks

--- R6TR/data/R6-Parametric-Endcone/parametricEndcone.py
+++ R6TR/data/R6-Parametric-Endcone/parametricEndcone.py
@@ -29,6 +29,10 @@ def plotAverages(averages):
     # Separate keys and average values into two lists for plotting
     keys = [item[0] for item in averages]
     values = [item[1] for item in averages]
+    for _ in range(3):
+        keys.pop(0)
+        values.pop(0)
+
 
     # Plot averages
     plt.plot(keys, values, 'o-')
@@ -49,15 +53,15 @@ def ForceToCD(force, velocity):
 def plotExampleValues(content):
     # Extract values from content
     y = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6]
-    x1 = [ForceToCD(float(content[i][2]), float(content[i][0])) for i in range(5, -1, -1)]
-    x2 = [ForceToCD(float(content[i][2]), float(content[i][0])) for i in range(29, 23, -1)]
-    x3 = [ForceToCD(float(content[i][2]), float(content[i][0])) for i in range(53, 47, -1)]
+    x3 = [ForceToCD(float(content[i][2]), float(content[i][0])) for i in range(77, 71, -1)]
+    x1 = [ForceToCD(float(content[i][2]), float(content[i][0])) for i in range(29, 23, -1)]
+    x2 = [ForceToCD(float(content[i][2]), float(content[i][0])) for i in range(53, 47, -1)]
 
     # Plot values
     plt.figure()  # Create a new figure
-    plt.plot(y, x1, 'o-', label='Angle of 0 degrees')
-    plt.plot(y, x2, 'o-', label='Angle of 4 degrees')
-    plt.plot(y, x3, 'o-', label='Angle of 8 degrees')
+    plt.plot(y, x1, 'o-', label='Angle of 4 degrees')
+    plt.plot(y, x2, 'o-', label='Angle of 8 degrees')
+    plt.plot(y, x3, 'o-', label='Angle of 12 degrees')
     plt.xlabel("Mach")
     plt.ylabel("Drag coefficient")
     plt.title('Cd Values for Different Angles of Endcone')
@@ -67,9 +71,13 @@ def plotExampleValues(content):
 def plotCdVsAngle(content):
     # Extract values from content
     y = [item[0] for item in averages]
-    x1 = [ForceToCD(float(content[i][2]), float(content[i][0])) for i in range(4, 95, 6)]
-    x2 = [ForceToCD(float(content[i][2]), float(content[i][0])) for i in range(2, 93, 6)]
-    x3 = [ForceToCD(float(content[i][2]), float(content[i][0])) for i in range(0, 91, 6)]
+    y.pop(0)
+    y.pop(0)
+    y.pop(0)
+    
+    x1 = [ForceToCD(float(content[i][2]), float(content[i][0])) for i in range(22, 95, 6)]
+    x2 = [ForceToCD(float(content[i][2]), float(content[i][0])) for i in range(20, 93, 6)]
+    x3 = [ForceToCD(float(content[i][2]), float(content[i][0])) for i in range(18, 91, 6)]
 
     # Plot values
     plt.figure()  # Create a new figure
